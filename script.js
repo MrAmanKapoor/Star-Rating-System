85ad7b98027c87de6287b68f2b274bd4f2594211
Aman Kapoor <amankapoor140@gmail.com>
2022-06-26 18:50:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+ratings = {
+    Pizza: 1.5,
+    Samosa: 3.8,
+    Pani_Puri: 5,
+    Pasta: 0
+}
+
+totalRating = 5;
+table = document.querySelector('.table');
+selectInput = document.getElementById('selectInput');
+ratingInput = document.getElementById('ratingInput');
+
+let phoneNames;
+selectInput.addEventListener('change', function(e) {
+    phoneNames = e.target.value;
+    ratingInput.disabled = false;
+    ratingInput.value = ratings[phoneNames];
+
+})
+
+ratingInput.addEventListener('change', function(e) {
+    ratingsinInput = e.target.value;
+    ratings[phoneNames] = ratingsinInput;
+    rateIt()
+})
+
+
+
+
+
+
+
+function rateIt() {
+
+    for (rating in ratings) {
+        ratingPercentage = ratings[rating]*100 / totalRating;
+        console.log("Rating$"+ ratingPercentage);
+        star = table.querySelector(`.${rating} .inner-star`);
+        numberRating = table.querySelector(`.${rating} .numberRating`);
+        ratingRounded=  ratings[rating]*100 / totalRating + "%";
+        star.style.width = ratingRounded;
+        numberRating.innerText = ratings[rating];
+
+    }
+}
+
+rateIt()
